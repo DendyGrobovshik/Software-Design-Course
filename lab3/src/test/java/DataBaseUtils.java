d34a902f46dadd71dd8de9ec484cf197ae6ffce4
DendyGrobovshik <idenis.gradoboev@yandex.com>
2022-10-27 12:41:29 +0300

    Add test for QueryServlet

--- lab3/src/test/java/DataBaseUtils.java
+++ lab3/src/test/java/DataBaseUtils.java
@@ -1,8 +1,34 @@
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DataBaseUtils {
+    public static String getMin() throws SQLException {
+        String sql = "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+        String header = "<h1>Product with min price: </h1>\n";
+
+        return executeQueryAndFormatForHTML(sql, header, appendNameAndPrice);
+    }
+
+    public static String getMax() throws SQLException {
+        String sql = "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+        String header = "<h1>Product with max price: </h1>\n";
+
+        return executeQueryAndFormatForHTML(sql, header, appendNameAndPrice);
+    }
+
+    public static String getSum() throws SQLException {
+        String sql = "SELECT SUM(price) as value FROM PRODUCT";
+        String header = "Summary price: \n";
+
+        return executeQueryAndFormatForHTML(sql, header, appendInt);
+    }
+
+    public static String getCount() throws SQLException {
+        String sql = "SELECT COUNT(*) as value FROM PRODUCT";
+        String header = "Number of products: \n";
+
+        return executeQueryAndFormatForHTML(sql, header, appendInt);
+    }
+
     public static int getNumberOfProductsByName(String name) throws SQLException {
         try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
             String sql = "SELECT COUNT(*) as COUNT "
@@ -41,4 +67,42 @@ public class DataBaseUtils {
             return sb.toString();
         }
     }
+
+    public static String executeQueryAndFormatForHTML(String sql, String header, ResultSetHandler rsHandler) throws SQLException {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("<html><body>\n");
+            sb.append(header);
+
+            rsHandler.handle(sb, rs);
+
+            sb.append("</body></html>\n");
+
+            stmt.close();
+
+            return sb.toString();
+        }
+    }
+
+    @FunctionalInterface
+    private interface ResultSetHandler {
+        void handle(StringBuilder sb, ResultSet rs) throws SQLException;
+    }
+
+    private static ResultSetHandler appendNameAndPrice = (StringBuilder sb, ResultSet rs) -> {
+        while (rs.next()) {
+            String name = rs.getString("name");
+            int price = rs.getInt("price");
+            sb.append(name).append("\t").append(price).append("</br>\n");
+        }
+    };
+
+    private static ResultSetHandler appendInt = (StringBuilder sb, ResultSet rs) -> {
+        while (rs.next()) {
+            sb.append(rs.getInt("value")).append('\n');
+        }
+    };
 }
---
+++ lab3/src/test/java/QueryServletTest.java
@@ -0,0 +1,97 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
+import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+public class QueryServletTest {
+    private HttpServletRequest request = mock(HttpServletRequest.class);
+    private HttpServletResponse response;
+    private StringWriter responseContent;
+
+    private static class QueryServletOpen extends QueryServlet {
+        @Override
+        public void doGet(HttpServletRequest request,
+                          HttpServletResponse response) throws IOException {
+            super.doGet(request, response);
+        }
+    }
+
+    @BeforeEach
+    void createResponseMock() throws IOException {
+        response = mock(HttpServletResponse.class);
+
+        responseContent = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(responseContent));
+    }
+
+    @Test
+    void min_request() throws IOException, SQLException {
+        when(request.getParameter("command")).thenReturn("min");
+
+        new QueryServletOpen().doGet(request, response);
+
+        assertEquals(
+                DataBaseUtils.getMin(),
+                responseContent.toString()
+        );
+    }
+
+    @Test
+    void max_request() throws IOException, SQLException {
+        when(request.getParameter("command")).thenReturn("max");
+
+        new QueryServletOpen().doGet(request, response);
+
+        assertEquals(
+                DataBaseUtils.getMax(),
+                responseContent.toString()
+        );
+    }
+
+    @Test
+    void sum_request() throws IOException, SQLException {
+        when(request.getParameter("command")).thenReturn("sum");
+
+        new QueryServletOpen().doGet(request, response);
+
+        assertEquals(
+                DataBaseUtils.getSum(),
+                responseContent.toString()
+        );
+    }
+
+    @Test
+    void count_request() throws IOException, SQLException {
+        when(request.getParameter("command")).thenReturn("count");
+
+        new QueryServletOpen().doGet(request, response);
+
+        assertEquals(
+                DataBaseUtils.getCount(),
+                responseContent.toString()
+        );
+    }
+
+    @Test
+    void unknown_request() throws IOException, SQLException {
+        when(request.getParameter("command")).thenReturn("do");
+
+        new QueryServletOpen().doGet(request, response);
+
+        assertEquals(
+                "Unknown command: do\n",
+                responseContent.toString()
+        );
+    }
+}
